b37d3e36f0c9e28f363c2c81b2cda351aae5d1ce
cpfarher <c.pfarher@gmail.com>
2014-06-05 16:18:47 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mongorails::Application.config.secret_key_base = 'b09ddb93e0eac455f7210951816c2d366e2c407d65966a76d1e0aae9ffed0132685c03c0f9b9ad51650181745bd941a46b0bc60bef292c4a10d67d090add6ee4'
